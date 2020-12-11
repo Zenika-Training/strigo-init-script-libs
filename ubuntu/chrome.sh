73237eda91c95e250489bab759174bf6223cb48b
Antoine Ceol <antoine.ceol@gmail.com>
2020-12-12 00:44:19 +0100

    :wrench: Install chrome for headless

---
+++ ubuntu/chrome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
