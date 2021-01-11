ee3c8c258eb836709b34dd7199540080ee97df51
Antoine Ceol <antoine.ceol@gmail.com>
2021-01-11 12:36:57 +0100

    Remove uneeded sudo
    Co-authored-by: Alexandre Garnier <zigarn@users.noreply.github.com>

--- ubuntu/chrome.sh
+++ ubuntu/chrome.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+
+# Set up the repository
+echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+# Install Chrome
+apt-get update
+apt-get install -y google-chrome-stable
