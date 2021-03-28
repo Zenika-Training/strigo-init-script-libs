686a771dac69604a87f9fe2ef1eb40a2ecd322e1
Luc Sorel-Giffo <luc.sorel@zenika.com>
2021-03-28 18:45:31 +0200

    made customizable  the code-server installation by defining optional variables at its beginning

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+# failfast
+set -e
+
+# Define these 2 variables if you want to customize the default installation,
+# then copy-paste the remainder of the script:
+# - the version of code-server to install
+# code_server_version=3.7.2
+# - the code-server extensions to install (space-separated names)
+# code_server_extensions="ms-azuretools.vscode-docker coenraads.bracket-pair-colorizer-2"
+
 # https://github.com/cdr/code-server/blob/master/doc/install.md#debian-ubuntu
 
 apt-get install -y curl
 
 # Install code-server
-curl -fsSLo /tmp/code-server.deb https://github.com/cdr/code-server/releases/download/v3.7.1/code-server_3.7.1_amd64.deb
+apt-get install -y curl
+code_server_version=${code_server_version-3.9.2}
+curl -fsSLo /tmp/code-server.deb "https://github.com/cdr/code-server/releases/download/v${code_server_version}/code-server_${code_server_version}_amd64.deb"
 apt-get install -y /tmp/code-server.deb
 
 # Setup code-server
 mkdir --parent /home/ubuntu/.config/code-server/
-cat <<EOF > /home/ubuntu/.config/code-server/config.yaml
+cat << EOF > /home/ubuntu/.config/code-server/config.yaml
 bind-addr: {{ .STRIGO_RESOURCE_DNS }}:9999
 auth: password
 password: '{{ .STRIGO_WORKSPACE_ID }}'
@@ -30,5 +42,10 @@ if [ $USER = ubuntu ]; then
 fi
 EOF
 
-# Install extensions
-#sudo -iu ubuntu code-server code-server --install-extension ...
+# Install extensions, if any
+if [[ $code_server_extensions && ${code_server_extensions-_} ]]; then
+  code_server_extensions_array=($code_server_extensions)
+  for code_server_extension in ${code_server_extensions_array[@]}; do
+    sudo -iu ubuntu code-server code-server --install-extension ${code_server_extension}
+  done
+fi
