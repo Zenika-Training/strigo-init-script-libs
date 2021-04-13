f29eb338884ac567f8d99f950472fa5aadef3445
Luc Sorel-Giffo <lucsorel@users.noreply.github.com>
2021-04-13 13:24:00 +0200

    Python basics - scripts tuning & recommendations (#8)
    * script which installs pyenv python & poetry and configures the labs folder
    
    * made customizable  the code-server installation by defining optional variables at its beginning
    
    * suggested some guidelines when writing strigo scripts
    
    * optional JSON user settings for code-server
    
    * install docker before code-server if you plan to use a docker extension
    
    * Update ubuntu/README.md
    
    handle cases where the variable is defined but empty
    
    Co-authored-by: Alexandre Garnier <zigarn@users.noreply.github.com>
    
    * add braces around variable
    
    Co-authored-by: Alexandre Garnier <zigarn@users.noreply.github.com>
    
    * add braces around variable name
    
    Co-authored-by: Alexandre Garnier <zigarn@users.noreply.github.com>
    
    * made the values optionally overrideable
    
    Co-authored-by: Alexandre Garnier <zigarn@users.noreply.github.com>
    
    * rebased master
    
    * handle defined-but-empty variables
    
    Co-authored-by: Alexandre Garnier <zigarn@users.noreply.github.com>
    
    Co-authored-by: Luc Sorel-Giffo <luc.sorel@zenika.com>
    Co-authored-by: Alexandre Garnier <zigarn@users.noreply.github.com>

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+# failfast
+set -e
+
+# Define these 2 variables if you want to customize the default installation,
+# then copy-paste the remainder of the script:
+# - the version of code-server to install
+# code_server_version=3.7.2
+# - the code-server extensions to install (space-separated names)
+# code_server_extensions="ms-azuretools.vscode-docker coenraads.bracket-pair-colorizer-2"
+# Note: install code-server after installing docker if you plan to use a docker extension
+# - the JSON content of code-server settings (here, to make the terminal a JSON shell)
+# (see https://code.visualstudio.com/docs/editor/integrated-terminal#_shell-arguments)
+# code_server_settings='{ "terminal.integrated.shellArgs.linux": ["-l"]}'
+
 # https://github.com/cdr/code-server/blob/master/doc/install.md#debian-ubuntu
 
 apt-get install -y curl
 
 # Install code-server
-curl -fsSLo /tmp/code-server.deb https://github.com/cdr/code-server/releases/download/v3.7.1/code-server_3.7.1_amd64.deb
+apt-get install -y curl
+code_server_version=${code_server_version:-3.9.2}
+curl -fsSLo /tmp/code-server.deb "https://github.com/cdr/code-server/releases/download/v${code_server_version}/code-server_${code_server_version}_amd64.deb"
 apt-get install -y /tmp/code-server.deb
 
 # Setup code-server
 mkdir --parent /home/ubuntu/.config/code-server/
-cat <<EOF > /home/ubuntu/.config/code-server/config.yaml
+cat << EOF > /home/ubuntu/.config/code-server/config.yaml
 bind-addr: {{ .STRIGO_RESOURCE_DNS }}:9999
 auth: password
 password: '{{ .STRIGO_WORKSPACE_ID }}'
@@ -30,5 +46,16 @@ if [ $USER = ubuntu ]; then
 fi
 EOF
 
-# Install extensions
-#sudo -iu ubuntu code-server code-server --install-extension ...
+# Install extensions, if any
+if [[ ${code_server_extensions} && ${code_server_extensions-_} ]]; then
+  code_server_extensions_array=($code_server_extensions)
+  for code_server_extension in ${code_server_extensions_array[@]}; do
+    sudo -iu ubuntu code-server code-server --install-extension ${code_server_extension}
+  done
+fi
+
+# Adds user settings, if any
+if [[ ${code_server_settings} && ${code_server_settings-_} ]]; then
+  echo ${code_server_settings} > /home/ubuntu/.local/share/code-server/User/settings.json
+  chown ubuntu:ubuntu /home/ubuntu/.local/share/code-server/User/settings.json
+fi
