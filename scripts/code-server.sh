a73cb89932aca29c96ffe42b68d88264b73d7e5c
Luc Sorel-Giffo <luc.sorel@zenika.com>
2021-06-25 11:20:38 +0200

    moved the logic of "displaying the warning in the trainer terminal only" outside of the /etc/profile.d/code-server-password.sh file (renamed into -terminal.sh)

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -18,16 +18,10 @@ set -e
 
 apt-get install -y curl
 
-# Install code-server
+# Install code-server (last released version by default)
 apt-get install -y curl
 last_code_server_release=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/cdr/code-server/releases/latest | grep -Po 'cdr/code-server/releases/tag/v\K[^"]*')
 code_server_version=${code_server_version:-${last_code_server_release}}
-# Defines the message about the code-server version displayed in the trainer terminal
-if [ "${code_server_version}" = "${last_code_server_release}" ]; then
-    code_server_version_message="The last release version of code server is installed (${last_code_server_release})"
-else
-    code_server_version_message="!!! Version ${code_server_version} of code server is installed, but a newer release exists (${last_code_server_release})"
-fi
 curl -fsSLo /tmp/code-server.deb "https://github.com/cdr/code-server/releases/download/v${code_server_version}/code-server_${code_server_version}_amd64.deb"
 apt-get install -y /tmp/code-server.deb
 
@@ -45,17 +39,29 @@ chown -R ubuntu: /home/ubuntu/.config/
 systemctl enable --now code-server@ubuntu
 
 # Display code-server password in terminal
-cat << EOF > /etc/profile.d/code-server-password.sh
-if [ \$USER = ubuntu ]; then
-  # displays the message about code-server version to the trainer only
-  if [ "{{ .STRIGO_USER_EMAIL }}" = "{{ .STRIGO_EVENT_HOST_EMAIL }}" ]; then
-    echo -ne "\n${code_server_version_message}. Trainees do not see this message."
-  fi
+cat <<\EOF > /etc/profile.d/code-server-terminal.sh
+if [ $USER = ubuntu ]; then
   echo -ne '\nCode-server '
   grep '^password:' ~/.config/code-server/config.yaml
   echo
 fi
 EOF
+
+# Displays the message about code-server version in the trainer terminal only
+if [ "{{ .STRIGO_USER_EMAIL }}" = "{{ .STRIGO_EVENT_HOST_EMAIL }}" ]; then
+  # defines the message about the code-server version
+  if [ "${code_server_version}" = "${last_code_server_release}" ]; then
+      code_server_version_message="The last release version of code server is installed (${last_code_server_release})"
+  else
+      code_server_version_message="!!! Version ${code_server_version} of code server is installed, but a newer release exists (${last_code_server_release})"
+  fi
+
+  cat << EOF >> /etc/profile.d/code-server-terminal.sh
+if [ \$USER = ubuntu ]; then
+  echo -e "${code_server_version_message}. Trainees do not see this message."
+fi
+EOF
+fi
 
 # Install extensions, if any
 if [[ ${code_server_extensions} && ${code_server_extensions-_} ]]; then
