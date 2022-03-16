00d6f0a418d06c42e1ef4d57267d4dfdee803c9a
Alexandre Garnier <alexandre.garnier@zenika.com>
2022-03-16 10:15:39 +0100

    Setup lock waiting in dpkg

--- scripts/ubuntu-20.04-apt-lock-fix.sh
+++ scripts/ubuntu-20.04-apt-lock-fix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Setup lock waiting in dpkg (15 minutes timeout instead of 0)
+echo 'DPkg::Lock::Timeout "900";' > /etc/apt/apt.conf.d/99-dpkg-timeout
+
 # Remove unattended-upgrades to avoid conflicts with apt running twice
 apt-get remove -y unattended-upgrades
