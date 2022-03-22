9d61dea4ac7b6b7c2c501a095f16121bf5737dc0
Alexandre Garnier <alexandre.garnier@zenika.com>
2022-03-22 11:10:42 +0100

    Create a retry wrapper for gdown

---
+++ scripts/materials-helper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get install -y --no-install-recommends python3 python3-pip
+pip3 install gdown
+
+GDOWN_RETRY_MAX=5
+
+function gdown {
+  local retries=${GDOWN_RETRY_MAX}
+  until [ ${retries} -lt 1 ]; do
+    command gdown $@
+    if [ $? -eq 0 ]; then
+      break
+    fi
+    retries=$(( retries - 1 ))
+    echo "gdown failed, retrying in 1 seconds..." >&2
+    sleep 1
+  done
+  if [ ${retries} -lt 1 ]; then
+    echo "gdown failed ${GDOWN_RETRY_MAX} times, giving up." >&2
+    return 1
+  fi
+}
