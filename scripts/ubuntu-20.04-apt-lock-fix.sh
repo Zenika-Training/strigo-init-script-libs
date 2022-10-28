87b517bb7ffe7368055edad987db3edc52e13311
Alexandre Garnier <zigarn@users.noreply.github.com>
2022-10-28 15:43:32 +0200

    Update expired Google's APT key
    To avoid error message on 'apt update'

--- scripts/ubuntu-20.04-apt-lock-fix.sh
+++ scripts/ubuntu-20.04-apt-lock-fix.sh
@@ -5,3 +5,6 @@ echo 'DPkg::Lock::Timeout "900";' > /etc/apt/apt.conf.d/99-dpkg-timeout
 
 # Remove unattended-upgrades to avoid conflicts with apt running twice
 apt-get remove -y unattended-upgrades
+
+# Update expired Google's APT key (to avoid error message on 'apt update')
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
