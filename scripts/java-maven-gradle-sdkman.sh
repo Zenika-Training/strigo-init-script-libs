8ed54ffc6eba02a6264830b14dfe739e3e1386a8
Patrice De Saint Steban <patrice@desaintsteban.fr>
2024-09-27 10:15:13 +0200

    Update java-maven-gradle-sdkman.sh

--- scripts/java-maven-gradle-sdkman.sh
+++ scripts/java-maven-gradle-sdkman.sh
@@ -5,10 +5,18 @@ apt-get purge default-jdk java-common
 apt-get install -y curl zip
 
 # Define these 3 optional variables if you want to customize the default installation,
-# then copy-paste the remainder of the script:
-java_version=17.0.0-tem
-mvn_version=3.8.3
-gradle_version=7.2
+# Add this in the strigo.json :
+# {
+#       "script": "java-maven-gradle-sdkman.sh",
+#       "env": {
+#         "java_version": "21-oracle",
+#         "mvn_version": "3.9.9",
+#         "gradle_version": "8.10",
+#        }
+#      },
+java_version=${java_version:-17.0.0-tem}
+mvn_version=${mvn_version:-3.8.3}
+gradle_version=${gradle_version:-7.2}
 
 # https://sdkman.io/install
 # Run install with ubuntu user
